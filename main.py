d2a658ae7b2c18e7fdce7eace5977c99dd4d32ab
Roman Yatsenko <roman.yatsenko@hneu.net>
2022-06-16 18:23:52 +0300

    Add start cmd and cancel action

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ import config
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
-async def echo(message: types.Message) -> None:
-    await message.reply(message.text)
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message) -> None:
+    """Start command handler"""
+    poll_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    poll_keyboard.add(types.KeyboardButton(
+        text='Создать тест',
+        request_poll=types.KeyboardButtonPollType(type=types.PollType.QUIZ)
+    ))
+    poll_keyboard.add(types.KeyboardButton(text='Отмена'))
+    await message.answer('Нажмите на кнопку и создайте тест!',
+                        reply_markup=poll_keyboard)
+
+@dp.message_handler(lambda message: message.text == 'Отмена')
+async def action_cancel(message: types.Message) -> None:
+    """Cancel action handler"""
+    await message.answer('Действие отменено. Введите /start, чтоы начать заново',
+        reply_markup=types.ReplyKeyboardRemove())
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
